8636e4152a5dbe4f9a8d8efb4df1e850ea60ef77
YOOOOOOKI <zyqzyqzyq106@gmail.com>
2017-08-15 02:52:19 +0800

    first commit

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,23 +9,48 @@ chai.use(sinonChai);
 var main = require("../lib/main.js");
 
 
-describe("测试描述", function(){
+describe("beerbottle liric test", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("测试用例1:有零瓶啤酒的情况", function(){
 
-        var result = main();
-        var expect_string = '';
+        var result = main(0);
+        var expect_string = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+    it("测试用例1:有1瓶啤酒的情况", function(){
 
+        var result = main(1);
+        var expect_string = `1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+        
         expect(expect_string).to.equal(result);
     });
+    
+    it("测试用例3:有5瓶啤酒的情况", function(){
+
+        var result = main(5);
+        var expect_string =`5 bottles of beer on the wall, 5 bottles of beer.
+Take one down and pass it around, 4 bottles of beer on the wall.
+4 bottles of beer on the wall, 4 bottles of beer.
+Take one down and pass it around, 3 bottles of beer on the wall.
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.`;
+        
+        expect(expect_string).to.equal(result);
+    });
+
+       
+   
 });
